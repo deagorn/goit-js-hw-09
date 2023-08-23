06ad829cfb2e0cf8600aba0934a8eba83dd93220
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-24 02:57:49 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  btn: document.querySelector('button'),
+  form: document.querySelector('form'),
+}
+
+refs.form.addEventListener('submit', onSubmitBtn);
+
+function onSubmitBtn(e) {
+  console.log(refs.btn);
+  e.preventDefault();
+  
+  const amountValue = Number(refs.amount.value);
+  const delayValue = Number(refs.delay.value);
+  const stepValue = Number(refs.step.value);
+  
+  for (let i = 1; i <= amountValue; i++) {
+    let totalDelay = delayValue + stepValue * (i - 1);
+    setTimeout(() => {
+      createPromise(i, totalDelay);
+    }, totalDelay);
+  };
+
+  refs.form.reset();
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+ const promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(Notify.success(`Fulfilled promise ${position} in ${delay}`));
+    } else {
+      reject(Notify.failure(`Rejected promise ${position} in ${delay}`));
+    }
+ });
+  return promise;
 }
