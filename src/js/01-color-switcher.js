691aed09c2797c302a4fd591f37c07b560e4bd67
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-23 23:42:48 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+refs.startBtn.addEventListener('click', onStartClick)
+refs.stopBtn.addEventListener('click', onStopClick)
+
+let timerId = null;
+
+
+function onStartClick() {
+    timerId = setInterval(ChangingColorPage, 1000);
+}
+
+function ChangingColorPage() {
+    const color = getRandomHexColor();
+    refs.body.style.backgroundColor = `${color}`;
+    refs.startBtn.disabled = true;
+}
+
+function onStopClick() {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
